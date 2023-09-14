f93faa25528b220820ee0a19dadefb273acfa517
Elena2485 <elena2485@yandex.ru>
2023-09-14 12:03:38 +0300

    demoqa.py

---
+++ tests/test_check_icon_demoqa.py
@@ -0,0 +1,23 @@
+from conftest import browser
+##from selenium.webdriver.common.by import By
+
+from pages.demoqa import DemoQa
+
+def test_icon_exist(browser):
+    demo_qa_page = DemoQa(browser)
+    demo_qa_page.visit()
+    demo_qa_page.click_on_the_icon()
+    assert demo_qa_page.equal_url()
+    assert demo_qa_page.exist_icon()
+# def click_on_the_icon():
+
+
+#def test_icon_exist(browser):
+#    browser.get('https://demoqa.com/')
+ #   icon = browser.find_element(By.CSS_SELECTOR, '#app > header > a')
+#    if icon is None:
+#        print("Элемент не найден")
+#    else:
+#        print("Элемент найден")
+
+
