728498a4193b39dab10115d4a899e56c1f8a3ea5
Elena2485 <elena2485@yandex.ru>
2023-09-15 03:27:29 +0300

    demoqa

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -1,5 +1,5 @@
-from conftest import browser
-##from selenium.webdriver.common.by import By
+#from conftest import browser
+#from selenium.webdriver.common.by import By
 
 from pages.demoqa import DemoQa
 def test_icon_exist(browser):
@@ -9,14 +9,10 @@ def test_icon_exist(browser):
     assert demo_qa_page.equal_url()
     assert demo_qa_page.exist_icon()
 
-# def click_on_the_icon():
+def equal_url(self):
+    if self.get_url() == 'https://demoqa.com/':
+       return True
+    return False
 
-#def test_icon_exist(browser):
-#    browser.get('https://demoqa.com/')
- #   icon = browser.find_element(By.CSS_SELECTOR, '#app > header > a')
-#    if icon is None:
-#        print("Элемент не найден")
-#    else:
-#        print("Элемент найден")
-
-
+def get_url(self):
+    return self.driver.current_url
